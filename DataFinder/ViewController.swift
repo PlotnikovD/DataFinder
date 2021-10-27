696f379fad9e26f72689b648eb932b87456ecede
D.Plotnikov <plotnikdima5296@gmail.com>
2021-10-27 11:05:39 +0500

    with code

--- DataFinder/ViewController.swift
+++ DataFinder/ViewController.swift
@@ -15,7 +15,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLable: UILabel!
     
     @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dataTextField.text, let month = monthTextField.text, let year = yearTextField.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormator = DateFormatter()
+        dateFormator.locale = Locale(identifier: "ru_Ru")
+        
+        dateFormator.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekDay = dateFormator.string(from: date)
+        let capitalizedWeekday = weekDay.capitalized
+        
+        resultLable.text = weekDay
+    }
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        }
     }
     
-}
 
